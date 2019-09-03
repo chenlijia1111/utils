fcd6ad114fd50068dc9c7da41c6e20cb83076c51
jiajia1997 <571740367@qq.com>
2019-09-03 13:05:29 +0800

    init

---
+++ src/main/java/com/github/chenlijia1111/utils/Main.java
@@ -0,0 +1,5 @@
+package com.github.chenlijia1111.utils;
+
+
+public class Main {
+}
---
+++ src/main/java/com/github/chenlijia1111/utils/core/StringUtils.java
@@ -0,0 +1,12 @@
+package com.github.chenlijia1111.utils.core;
+
+
+public class StringUtils {
+
+
+
+    public static boolean isEmpty(String str) {
+        return null == str || str.length() == 0;
+    }
+
+}
