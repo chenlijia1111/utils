26318e4d9b9d50ea01d2beeda209a986e0fc4bc4
571740367@qq.com <clj362321>
2020-11-05 11:16:56 +0800

    bigDecimal计算

--- src/main/java/com/github/chenlijia1111/utils/core/BigDecimalUtil.java
+++ src/main/java/com/github/chenlijia1111/utils/core/BigDecimalUtil.java
@@ -5,6 +5,7 @@ import java.math.BigDecimal;
 /**
  * bigDecimal 工具类
  * double 转 bigDecimal 最好先将 double 转为 字符串
+ *
  * @author Chen LiJia
  * @since 2020/9/30
  */
@@ -17,6 +18,7 @@ public class BigDecimalUtil {
 
     /**
      * 加法
+     *
      * @param v1
      * @param v2
      * @return
@@ -29,6 +31,7 @@ public class BigDecimalUtil {
 
     /**
      * 加法
+     *
      * @param v1
      * @param v2
      * @return
@@ -39,6 +42,7 @@ public class BigDecimalUtil {
 
     /**
      * 减法
+     *
      * @param v1
      * @param v2
      * @return
@@ -51,6 +55,7 @@ public class BigDecimalUtil {
 
     /**
      * 减法
+     *
      * @param v1
      * @param v2
      * @return
@@ -61,6 +66,7 @@ public class BigDecimalUtil {
 
     /**
      * 乘法
+     *
      * @param v1
      * @param v2
      * @return
@@ -73,6 +79,7 @@ public class BigDecimalUtil {
 
     /**
      * 乘法
+     *
      * @param v1
      * @param v2
      * @return
@@ -87,6 +94,7 @@ public class BigDecimalUtil {
 
     /**
      * 除法
+     *
      * @param v1
      * @param v2
      * @return
@@ -99,6 +107,7 @@ public class BigDecimalUtil {
 
     /**
      * 除法
+     *
      * @param v1
      * @param v2
      * @return
@@ -106,7 +115,7 @@ public class BigDecimalUtil {
     public static BigDecimal divide(BigDecimal v1, BigDecimal v2) {
         v1 = v1.setScale(SCALE, ROUNDING_MODE);
         v2 = v2.setScale(SCALE, ROUNDING_MODE);
-        BigDecimal multiply = v1.divide(v2);
+        BigDecimal multiply = v1.divide(v2, 2, ROUNDING_MODE);
         multiply = multiply.setScale(SCALE, ROUNDING_MODE);
         return multiply;
     }
