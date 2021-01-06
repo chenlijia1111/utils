3b476116f3dd59e0e04c495bc36d4c41e08571fa
571740367@qq.com <clj362321>
2021-01-06 15:40:26 +0800

    文件解压，删除文件

--- src/main/java/com/github/chenlijia1111/utils/core/FileUtils.java
+++ src/main/java/com/github/chenlijia1111/utils/core/FileUtils.java
@@ -3,16 +3,19 @@ package com.github.chenlijia1111.utils.core;
 
 import com.github.chenlijia1111.utils.common.AssertUtil;
 import com.github.chenlijia1111.utils.common.Result;
+import com.github.chenlijia1111.utils.list.Lists;
+import org.apache.commons.compress.archivers.zip.ZipArchiveEntry;
+import org.apache.commons.compress.archivers.zip.ZipFile;
+import org.apache.commons.compress.utils.SeekableInMemoryByteChannel;
 import org.slf4j.Logger;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.*;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Objects;
-import java.util.Set;
+import java.nio.charset.Charset;
+import java.util.*;
 import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 import java.util.zip.ZipOutputStream;
 
 /**
@@ -265,7 +268,7 @@ public class FileUtils {
                 zipOutputStream.putNextEntry(zipEntry);
                 BufferedInputStream bufferedInputStream = new BufferedInputStream(new FileInputStream(file));
                 byte[] bs = new byte[4096];
-                int read = -1;
+                int read;
                 while ((read = bufferedInputStream.read(bs, 0, 4096)) != -1) {
                     zipOutputStream.write(bs, 0, read);
                 }
@@ -282,5 +285,166 @@ public class FileUtils {
         return Result.failure("压缩失败");
     }
 
+    /**
+     * 解压压缩包
+     * 只能解压 zip 文件
+     * 调用此方法前先调用 {@link #checkZipEncoding(InputStream, Charset)} 判断编码
+     * 输入流需要重新获取，输入流用于判断之后就不能重复用了
+     * <p>
+     * 测试代码如下：
+     * {@code
+     * String zipFileName = "D:\\公司资料\\企业办公信息化\\批量导入票据资料.zip";
+     * try {
+     * FileInputStream fileInputStream = new FileInputStream(zipFileName);
+     * Charset charset = Charset.forName("UTF-8");
+     * boolean b = checkZipEncoding(fileInputStream, charset);
+     * if (!b){
+     * charset = Charset.forName("GBK");
+     * }
+     * fileInputStream = new FileInputStream(zipFileName);
+     * unzip(fileInputStream,charset,"D:\\公司资料\\企业办公信息化\\批量导入票据资料2");
+     * } catch (FileNotFoundException e) {
+     * e.printStackTrace();
+     * }
+     * }
+     *
+     * @param inputStream   输入流
+     * @param charset       编码
+     * @param destDirectory 解压路径
+     * @return
+     */
+    public static Result unzip(InputStream inputStream, Charset charset, String destDirectory) {
+
+        if (Objects.isNull(inputStream)) {
+            return Result.failure("输入流为空");
+        }
+        if (Objects.isNull(charset)) {
+            return Result.failure("编码为空");
+        }
+        if (StringUtils.isEmpty(destDirectory)) {
+            return Result.failure("解压路径为空");
+        }
+
+        byte[] bytes = IOUtil.readToBytes(inputStream);
+        SeekableInMemoryByteChannel memoryByteChannel = new SeekableInMemoryByteChannel(bytes);
+        try {
+            ZipFile zipFile = new ZipFile(memoryByteChannel, charset.name());
+            Enumeration<ZipArchiveEntry> entries = zipFile.getEntries();
+            while (entries.hasMoreElements()) {
+                ZipArchiveEntry zipArchiveEntry = entries.nextElement();
+                if (zipArchiveEntry.isDirectory()) {
+                    // 是文件夹
+                    // 文件夹，直接忽略就行了，因为文件是带了上级文件夹的名称的，
+                    // 到时候判断一下上级是否存在，不存在就创建就行了
+                    continue;
+                } else {
+                    // 是文件
+                    String name = zipArchiveEntry.getName();
+                    File file = new File(destDirectory + "/" + name);
+                    // 判断上级文件夹是否存在
+                    if (!file.getParentFile().exists()) {
+                        file.getParentFile().mkdirs();
+                    }
+
+                    // 输出文件
+                    InputStream fileItemInputStream = zipFile.getInputStream(zipArchiveEntry);
+                    FileOutputStream outputStream = new FileOutputStream(file);
+                    IOUtil.writeInputStream(fileItemInputStream, outputStream);
+                }
+            }
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            return Result.failure("解压失败");
+        }
+        return Result.success();
+    }
+
+    /**
+     * 测试压缩文件编码
+     * 源编码 测试编码参数
+     * GBK -> GBK true
+     * GBK -> UTF-8 false  使用这个来进行判断
+     * UTF-8 -> UTF-8 true
+     * UTF-8 -> UTF-8 true
+     * <p>
+     * 因为输入流是只能读取一次的，所以输入流要做好控制
+     * 如果是直接从文件获取的，其他地方就需要再获取一次
+     * 如果是通过 上传文件获取的，可以直接重新 get 一次输入流
+     *
+     * @param charset
+     * @return
+     */
+    public static boolean checkZipEncoding(InputStream fis, Charset charset) {
+
+        BufferedInputStream bis = new BufferedInputStream(fis);
+        ZipInputStream zis = new ZipInputStream(bis, charset);
+        try {
+            while (zis.getNextEntry() != null) {
+                // do nothing
+            }
+        } catch (Exception e) {
+            return false;
+        } finally {
+            IOUtil.close(zis, bis, fis);
+        }
+        return true;
+    }
+
+    /**
+     * 查询文件夹下的所有文件列表
+     * 递归
+     *
+     * @param directoryPath
+     * @return
+     */
+    public static List<File> listAllFileByDirectory(String directoryPath) {
+        List<File> fileList = new ArrayList<>();
+        if (StringUtils.isNotEmpty(directoryPath)) {
+            File file = new File(directoryPath);
+            if (file.exists() && file.isDirectory()) {
+                // 开始处理
+                // 查询所有下级文件
+                File[] files = file.listFiles();
+                if (null != files && files.length > 0) {
+                    for (int i = 0; i < files.length; i++) {
+                        File childFile = files[i];
+                        if (childFile.isDirectory()) {
+                            // 是文件夹，递归处理
+                            List<File> childFileList = listAllFileByDirectory(childFile.getAbsolutePath());
+                            if (Lists.isNotEmpty(childFileList)) {
+                                fileList.addAll(childFileList);
+                            }
+                        } else {
+                            // 是文件
+                            fileList.add(childFile);
+                        }
+                    }
+                }
+            }
+        }
+        return fileList;
+    }
+
+    /**
+     * 删除文件或文件夹
+     *
+     * @param file
+     */
+    public static void deleteFile(File file) {
+        if (Objects.nonNull(file) && file.exists()) {
+            if (file.isDirectory()) {
+                // 是文件夹，先删除下级
+                File[] files = file.listFiles();
+                if (Objects.nonNull(files) && files.length > 0) {
+                    for (int i = 0; i < files.length; i++) {
+                        File childFile = files[i];
+                        deleteFile(childFile);
+                    }
+                }
+            }
+            // 删除
+            file.delete();
+        }
+    }
 
 }
